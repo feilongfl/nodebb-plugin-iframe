fad6911d73d681b8d7f3750fef4fdc2c21a38bf8
feilong <feilongphone@gmail.com>
2017-11-13 23:03:33 +0800

    bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug bug

--- public/src/babylon-gltf.js
+++ public/src/babylon-gltf.js
@@ -7,8 +7,11 @@ function setGltf(gltfarea) {
 	console.log('root: ' + rooturl + ' ,name: ' + gltfname);
 
 	var canvas = document.createElement("CANVAS");
-
+	canvas.width = 400;
+	canvas.height = 400;
 	gltfarea.appendChild(canvas);
+
+	var engine = new BABYLON.Engine(canvas, true);
 
 	var createScene = function () {
 		// This creates a basic Babylon Scene object (non-mesh)
@@ -30,18 +33,30 @@ function setGltf(gltfarea) {
 
 		// Append sample glTF model to scene
 		BABYLON.SceneLoader.Append("https://raw.githubusercontent.com/feilongfl/pic-bed/master/201711/", "%E6%BB%91%E7%A8%BD.glb", scene, function (scene) {}, null, function (scene) {
-			alert("error");
+			console.log("error!")
 		});
 
 		return scene;
 	};
+
+	var scene = createScene();
+
+	engine.runRenderLoop(function () {
+		scene.render();
+	});
+
+	window.addEventListener("resize", function () { // Watch for browser/canvas resize events
+		engine.resize();
+	});
 }
 
 
-// require(['https://cdnjs.cloudflare.com/ajax/libs/babylonjs/2.5.0/babylon.js'], function () {
-require(['https://cdnjs.cloudflare.com/ajax/libs/babylonjs/3.0.1-beta/babylon.max.js'], function () {
-	for (var i = 0; i < $(".gltfobj").length; i++) {
-		setGltf($(".gltfobj")[i]);
-		// console.log(i);
-	}
+// require(['https://cdnjs.cloudflare.com/ajax/libs/babylonjs/3.0.1-beta/babylon.max.js'], function () {
+require(['https://cdn.babylonjs.com/babylon.js'], function () {
+	require(['https://cdn.rawgit.com/BabylonJS/Babylon.js/master/dist/preview%20release/loaders/babylon.glTFFileLoader.min.js'], function () {
+		for (var i = 0; i < $(".gltfobj").length; i++) {
+			setGltf($(".gltfobj")[i]);
+			// console.log(i);
+		}
+	});
 });
