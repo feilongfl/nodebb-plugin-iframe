d4f74e1c655bba4de25fb420990c46b08dbefad5
feilong <feilongphone@gmail.com>
2017-11-15 08:03:54 +0800

    fix height

--- public/src/iframeloader.js
+++ public/src/iframeloader.js
@@ -7,11 +7,13 @@ $(window).on('action:ajaxify.end', function () {
 			iframehtml.id = 'iframe';
 			$(".iframe-content")[i].appendChild(iframehtml);
 			$("#iframe")[i].width = $(".iframe-content")[i].clientWidth;
+			$("#iframe")[i].height=$("#iframe")[i].width * 9 / 16
 		}
 
 		window.addEventListener("resize", function () { // Watch for browser/canvas resize events
 			for (var i = 0; i < $(".iframe-content").length; i++) {
 				$("#iframe")[i].width = $(".iframe-content")[i].clientWidth
+				$("#iframe")[i].height=$("#iframe")[i].width * 9 / 16
 			}
 		});
 	}
