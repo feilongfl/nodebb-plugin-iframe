19a3abcdd26361943595fb93ac991ec2843e8a3b
feilong <feilongphone@gmail.com>
2017-11-15 16:25:54 +0800

    fix max-width

--- public/src/iframeloader.js
+++ public/src/iframeloader.js
@@ -7,13 +7,14 @@ $(window).on('action:ajaxify.end', function () {
 			iframehtml.id = 'iframe';
 			$(".iframe-content")[i].appendChild(iframehtml);
 			$("#iframe")[i].width = $(".iframe-content")[i].clientWidth;
-			$("#iframe")[i].height=$("#iframe")[i].width * 9 / 16
+			$("#iframe")[i].height = $("#iframe")[i].width * 9 / 16;
+			$("#iframe")[i].style.maxWidth = "100%";
 		}
 
 		window.addEventListener("resize", function () { // Watch for browser/canvas resize events
 			for (var i = 0; i < $(".iframe-content").length; i++) {
 				$("#iframe")[i].width = $(".iframe-content")[i].clientWidth
-				$("#iframe")[i].height=$("#iframe")[i].width * 9 / 16
+				$("#iframe")[i].height = $("#iframe")[i].width * 11 / 16
 			}
 		});
 	}
